b79a3725d2c9ab71a3a43efe4c64fcbd540df601
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-06-24 10:16:04 -0600

    recomendacion diaria

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -29,7 +29,8 @@ class DatabaseManager {
       print('No se ha conectado a la base de datos!!!!: $e');
     }
   }
-static Future<void> insertEvento(int consultorioId, Evento evento) async {
+
+  static Future<void> insertEvento(int consultorioId, Evento evento) async {
     try {
       final conn = await _connect();
 
@@ -63,6 +64,7 @@ static Future<void> insertEvento(int consultorioId, Evento evento) async {
       print('Error al insertar el evento: $e');
     }
   }
+
   static Future<List<Map<String, dynamic>>> getConsultoriosData() async {
     List<Map<String, dynamic>> consultoriosData = [];
     try {
@@ -376,8 +378,6 @@ static Future<void> insertEvento(int consultorioId, Evento evento) async {
     }
   }
 
-  
-
   static Future<List<Map<String, dynamic>>> getEventosData(
       int consultorioId) async {
     List<Map<String, dynamic>> eventos = [];
@@ -466,6 +466,237 @@ static Future<void> insertEvento(int consultorioId, Evento evento) async {
     return usuarios;
   }
 
+  static Future<List<Map<String, dynamic>>> getRecomeDiaria() async {
+    List<Map<String, dynamic>> recomeDiaria = [];
+    try {
+      final conn = await _connect();
+      final result = await conn.execute("""
+      WITH fechas AS (
+          SELECT 
+              CURRENT_DATE + i AS recomendacion_semanal,
+              LOWER(translate(TO_CHAR(CURRENT_DATE + i, 'TMDay'), 'ÁÉÍÓÚáéíóú', 'AEIOUaeiou')) AS dia_de_la_semana,
+              TO_CHAR(CURRENT_TIMESTAMP, 'HH24:MI') AS hora_actual
+          FROM 
+              generate_series(0, 6) AS s(i)
+      ),
+      horario AS (
+          SELECT 
+              id,
+              'lunes' AS dia_de_la_semana,
+              UNNEST(string_to_array(lunes, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'martes' AS dia_de_la_semana,
+              UNNEST(string_to_array(martes, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'miercoles' AS dia_de_la_semana,
+              UNNEST(string_to_array(miercoles, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'jueves' AS dia_de_la_semana,
+              UNNEST(string_to_array(jueves, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'viernes' AS dia_de_la_semana,
+              UNNEST(string_to_array(viernes, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'sabado' AS dia_de_la_semana,
+              UNNEST(string_to_array(sabado, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'domingo' AS dia_de_la_semana,
+              UNNEST(string_to_array(domingo, ',')) AS hora
+          FROM horario_consultorio
+      ),
+      eventos AS (
+          SELECT 
+              DATE(fecha_inicio) AS fecha_evento,
+              to_char(fecha_inicio, 'HH24:MI') AS hora_inicio,
+              to_char(fecha_fin, 'HH24:MI') AS hora_fin
+          FROM 
+              evento
+      ),
+      horas_libres AS (
+          SELECT 
+              f.recomendacion_semanal,
+              f.dia_de_la_semana,
+              SUBSTR(h.hora, 1, 5) AS hora_disponible
+          FROM 
+              fechas f
+          JOIN 
+              horario h
+          ON 
+              f.dia_de_la_semana = h.dia_de_la_semana
+          LEFT JOIN 
+              eventos e
+          ON 
+              f.recomendacion_semanal = e.fecha_evento
+              AND (
+                  (split_part(h.hora, '-', 1) BETWEEN e.hora_inicio AND e.hora_fin)
+                  OR (split_part(h.hora, '-', 2) BETWEEN e.hora_inicio AND e.hora_fin)
+                  OR (e.hora_inicio BETWEEN split_part(h.hora, '-', 1) AND split_part(h.hora, '-', 2))
+                  OR (e.hora_fin BETWEEN split_part(h.hora, '-', 1) AND split_part(h.hora, '-', 2))
+              )
+          WHERE 
+              e.fecha_evento IS NULL
+              AND (f.recomendacion_semanal > CURRENT_DATE OR (f.recomendacion_semanal = CURRENT_DATE AND split_part(h.hora, '-', 1) >= f.hora_actual))
+          ORDER BY 
+              f.recomendacion_semanal, h.hora
+      )
+      SELECT 
+          TO_CHAR(recomendacion_semanal, 'YYYY-MM-DD') AS recomendacion_semanal,
+          dia_de_la_semana,
+          hora_disponible
+      FROM 
+          horas_libres
+      LIMIT 1;
+    """);
+      for (var row in result) {
+        recomeDiaria.add({
+          'fecha': row[0],
+          'dia': row[1],
+          'hora': row[2],
+        });
+      }
+      await conn.close();
+    } catch (e) {
+      print('Error: $e');
+    }
+    return recomeDiaria;
+  }
+  
+
+
+
+
+  static Future<List<Map<String, dynamic>>> getRecomeSema() async {
+    List<Map<String, dynamic>> recomeSema = [];
+    try {
+      final conn = await _connect();
+      final result = await conn.execute("""
+      WITH fechas AS (
+          SELECT 
+              CURRENT_DATE + i AS recomendacion_semanal,
+              LOWER(translate(TO_CHAR(CURRENT_DATE + i, 'TMDay'), 'ÁÉÍÓÚáéíóú', 'AEIOUaeiou')) AS dia_de_la_semana,
+              TO_CHAR(CURRENT_TIMESTAMP, 'HH24:MI') AS hora_actual
+          FROM 
+              generate_series(7, 30) AS s(i)
+      ),
+      horario AS (
+          SELECT 
+              id,
+              'lunes' AS dia_de_la_semana,
+              UNNEST(string_to_array(lunes, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'martes' AS dia_de_la_semana,
+              UNNEST(string_to_array(martes, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'miercoles' AS dia_de_la_semana,
+              UNNEST(string_to_array(miercoles, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'jueves' AS dia_de_la_semana,
+              UNNEST(string_to_array(jueves, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'viernes' AS dia_de_la_semana,
+              UNNEST(string_to_array(viernes, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'sabado' AS dia_de_la_semana,
+              UNNEST(string_to_array(sabado, ',')) AS hora
+          FROM horario_consultorio
+          UNION ALL
+          SELECT 
+              id,
+              'domingo' AS dia_de_la_semana,
+              UNNEST(string_to_array(domingo, ',')) AS hora
+          FROM horario_consultorio
+      ),
+      eventos AS (
+          SELECT 
+              DATE(fecha_inicio) AS fecha_evento,
+              to_char(fecha_inicio, 'HH24:MI') AS hora_inicio,
+              to_char(fecha_fin, 'HH24:MI') AS hora_fin
+          FROM 
+              evento
+      ),
+      horas_libres AS (
+          SELECT 
+              f.recomendacion_semanal,
+              f.dia_de_la_semana,
+              SUBSTR(h.hora, 1, 5) AS hora_disponible
+          FROM 
+              fechas f
+          JOIN 
+              horario h
+          ON 
+              f.dia_de_la_semana = h.dia_de_la_semana
+          LEFT JOIN 
+              eventos e
+          ON 
+              f.recomendacion_semanal = e.fecha_evento
+              AND (
+                  (split_part(h.hora, '-', 1) BETWEEN e.hora_inicio AND e.hora_fin)
+                  OR (split_part(h.hora, '-', 2) BETWEEN e.hora_inicio AND e.hora_fin)
+                  OR (e.hora_inicio BETWEEN split_part(h.hora, '-', 1) AND split_part(h.hora, '-', 2))
+                  OR (e.hora_fin BETWEEN split_part(h.hora, '-', 1) AND split_part(h.hora, '-', 2))
+              )
+          WHERE 
+              e.fecha_evento IS NULL
+              AND (f.recomendacion_semanal > CURRENT_DATE OR (f.recomendacion_semanal = CURRENT_DATE AND split_part(h.hora, '-', 1) >= f.hora_actual))
+          ORDER BY 
+              f.recomendacion_semanal, h.hora
+      )
+      SELECT 
+          TO_CHAR(recomendacion_semanal, 'YYYY-MM-DD') AS recomendacion_semanal,
+          dia_de_la_semana,
+          hora_disponible
+      FROM 
+          horas_libres
+      LIMIT 1;
+    """);
+      for (var row in result) {
+        recomeSema.add({
+          'fecha': row[0],
+          'dia': row[1],
+          'hora': row[2],
+        });
+      }
+      await conn.close();
+    } catch (e) {
+      print('Error: $e');
+    }
+    return recomeSema;
+  }
+  
+
   static Future<List<Map<String, dynamic>>> getPacientes() async {
     List<Map<String, dynamic>> pacientes = [];
     try {
@@ -492,8 +723,6 @@ static Future<void> insertEvento(int consultorioId, Evento evento) async {
     }
     return pacientes;
   }
-
-
 
   static Future<void> deletePaciente(int id) async {
     try {

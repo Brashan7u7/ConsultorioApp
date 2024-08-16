a88faceeb50aff553cd5e940b11b110926360670
Brayan Noé <84888260+Brashan7u7@users.noreply.github.com>
2024-08-16 11:57:50 -0600

    cambios

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -13,7 +13,7 @@ class DatabaseManager {
     tz.setLocalLocation(tz.getLocation('America/Mexico_City'));
     return await Connection.open(
       Endpoint(
-        host: '127.0.0.1',
+        host: '192.168.1.182',
         //host: '192.168.1.181',
         port: 5432,
         database: 'medicalmanik',
